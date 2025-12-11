f6b2f1142f7fca699a78fef8552dda07861cee0f
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-12-11 22:34:52 +0530

    feat(mesh): added indexed mesh replacing triangle soup

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,9 +1,11 @@
-pub mod indexed_mesh;
 pub mod obj;
 pub mod stl;
 
-use crate::model::indexed_mesh::IndexedMesh;
+use std::ops::Range;
+
 use nalgebra::Vector3;
+use rayon::iter::{IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator};
+use smallvec::SmallVec;
 
 pub const MAX_TRIANGLES: u32 = 1_000_000;
 
@@ -109,16 +111,199 @@ impl Format {
             Self::OBJ => "obj",
         }
     }
+
+    pub fn get_codec(&self) -> Box<dyn MeshCodec> {
+        match self {
+            Self::STL => Box::new(stl::StlCodec),
+            Self::OBJ => Box::new(obj::ObjCodec),
+        }
+    }
 }
 
-pub trait MeshParser {
-    fn parse(bytes: &[u8]) -> anyhow::Result<Vec<Triangle>, anyhow::Error>;
-    fn write(path: &std::path::Path, triangles: &[Triangle]) -> anyhow::Result<(), anyhow::Error>;
+#[derive(Debug, Clone)]
+pub struct Mesh {
+    // list of all vertices
+    pub vertices: Vec<Vec3>,
+
+    // list of all vertex normals
+    // used to define smooth shading (how light interacts with the surface)
+    pub normals: Vec<Vec3>,
+    // list of all vertex texture coordinates
+    // used to map 2D images to the 3D surface
+    pub textures: Vec<Vec2>,
+
+    // list of all the faces
+    pub faces: Vec<Face>,
+    // groups of faces
+    // used to organize the mesh into logical sections
+    // e.g., wheels of a car
+    pub groups: Vec<Group>,
+
+    // material libraries associated with the mesh
+    pub matlibs: Vec<String>,
 }
 
-pub trait Mesh {
-    fn bounds(&self) -> anyhow::Result<[(f32, f32, f32); 2], anyhow::Error>;
-    fn diagonal(&self) -> anyhow::Result<f32, anyhow::Error>;
+impl Mesh {
+    pub fn scale(&mut self, target_diagonal: f32) -> anyhow::Result<()> {
+        let (min_vertex, max_vertex) = self.bounds()?;
+
+        let dx = max_vertex.0 - min_vertex.0;
+        let dy = max_vertex.1 - min_vertex.1;
+        let dz = max_vertex.2 - min_vertex.2;
+
+        let current_diagonal = (dx * dx + dy * dy + dz * dz).sqrt();
+        if current_diagonal == 0.0 {
+            return Err(anyhow::anyhow!("mesh has 0 dimensions"));
+        }
+
+        let center_x = (min_vertex.0 + max_vertex.0) / 2.0;
+        let center_y = (min_vertex.1 + max_vertex.1) / 2.0;
+        let center_z = (min_vertex.2 + max_vertex.2) / 2.0;
+
+        let scale_factor = target_diagonal / current_diagonal;
+
+        self.vertices.par_iter_mut().for_each(|vertex| {
+            vertex.0 = (vertex.0 - center_x) * scale_factor + center_x;
+            vertex.1 = (vertex.1 - center_y) * scale_factor + center_y;
+            vertex.2 = (vertex.2 - center_z) * scale_factor + center_z;
+        });
+
+        Ok(())
+    }
+
+    pub fn triangle_count(&self) -> usize {
+        self.faces
+            .iter()
+            .map(|face| face.v.len().saturating_sub(2))
+            .sum()
+    }
+}
+
+impl Mesh {
+    pub fn new() -> Self {
+        Self {
+            vertices: Vec::new(),
+            normals: Vec::new(),
+            textures: Vec::new(),
+            faces: Vec::new(),
+            groups: Vec::new(),
+            matlibs: Vec::new(),
+        }
+    }
+
+    #[inline]
+    pub fn bounds(&self) -> anyhow::Result<(Vec3, Vec3), anyhow::Error> {
+        if self.vertices.is_empty() {
+            return Err(anyhow::anyhow!("mesh has no vertices"));
+        }
+
+        let (min_vertex, max_vertex) = self
+            .vertices
+            .par_iter()
+            .fold(
+                || {
+                    (
+                        Vec3(f32::MAX, f32::MAX, f32::MAX),
+                        Vec3(f32::MIN, f32::MIN, f32::MIN),
+                    )
+                },
+                |acc, vertex| {
+                    (
+                        Vec3(
+                            acc.0.0.min(vertex.0),
+                            acc.0.1.min(vertex.1),
+                            acc.0.2.min(vertex.2),
+                        ),
+                        Vec3(
+                            acc.1.0.max(vertex.0),
+                            acc.1.1.max(vertex.1),
+                            acc.1.2.max(vertex.2),
+                        ),
+                    )
+                },
+            )
+            .reduce(
+                || {
+                    (
+                        Vec3(f32::MAX, f32::MAX, f32::MAX),
+                        Vec3(f32::MIN, f32::MIN, f32::MIN),
+                    )
+                },
+                |a, b| {
+                    (
+                        Vec3(a.0.0.min(b.0.0), a.0.1.min(b.0.1), a.0.2.min(b.0.2)),
+                        Vec3(a.1.0.max(b.1.0), a.1.1.max(b.1.1), a.1.2.max(b.1.2)),
+                    )
+                },
+            );
+
+        Ok((min_vertex, max_vertex))
+    }
+
+    pub fn diagonal(&self) -> anyhow::Result<f32, anyhow::Error> {
+        let (min_vertex, max_vertex) = self.bounds()?;
+
+        let dx = max_vertex.0 - min_vertex.0;
+        let dy = max_vertex.1 - min_vertex.1;
+        let dz = max_vertex.2 - min_vertex.2;
+
+        let current_diagonal = (dx * dx + dy * dy + dz * dz).sqrt();
+        if current_diagonal == 0.0 {
+            return Err(anyhow::anyhow!("mesh has 0 dimensions"));
+        }
+
+        Ok(current_diagonal)
+    }
+}
+
+impl Default for Mesh {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Face {
+    // vertex indices
+    pub v: SmallVec<[usize; 4]>,
+    // vertex normal indices
+    pub vn: SmallVec<[usize; 4]>,
+    // vertex texture indices
+    pub vt: SmallVec<[usize; 4]>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Group {
+    // group name
+    // e.g., "wheel", "door"
+    pub name: String,
+
+    // material name used by this group
+    pub material: Option<String>,
+
+    // range of faces in this group
+    pub face_range: Range<usize>,
+}
+
+pub trait MeshCodec {
+    fn parse(&self, bytes: &[u8]) -> anyhow::Result<Mesh>;
+    fn write(&self, path: &std::path::Path, mesh: &Mesh) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Triangle {
+    pub vertices: [Vec3; 3],
+}
+
+impl Triangle {
+    #[inline]
+    pub fn signed_volume(&self) -> f64 {
+        let a: Vector3<f64> = self.vertices[0].into();
+        let b: Vector3<f64> = self.vertices[1].into();
+        let c: Vector3<f64> = self.vertices[2].into();
+
+        (a.dot(&b.cross(&c))) / 6.0
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -186,68 +371,5 @@ impl Vec2 {
 
     pub fn dot(self, other: Vec2) -> f32 {
         self.0 * other.0 + self.1 * other.1
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Triangle {
-    pub vertices: [Vec3; 3],
-}
-
-impl From<&IndexedMesh> for Vec<Triangle> {
-    fn from(mesh: &IndexedMesh) -> Self {
-        mesh.faces
-            .iter()
-            .map(|face| Triangle {
-                vertices: [
-                    mesh.vertices[face[0]],
-                    mesh.vertices[face[1]],
-                    mesh.vertices[face[2]],
-                ],
-            })
-            .collect()
-    }
-}
-
-impl From<&mut IndexedMesh> for Vec<Triangle> {
-    fn from(mesh: &mut IndexedMesh) -> Self {
-        Vec::from(&*mesh)
-    }
-}
-
-impl Triangle {
-    pub fn signed_volume(&self) -> f64 {
-        let a: Vector3<f64> = self.vertices[0].into();
-        let b: Vector3<f64> = self.vertices[1].into();
-        let c: Vector3<f64> = self.vertices[2].into();
-
-        (a.dot(&b.cross(&c))) / 6.0
-    }
-
-    pub fn is_valid(&self) -> bool {
-        let v0 = self.vertices[0];
-        let v1 = self.vertices[1];
-        let v2 = self.vertices[2];
-
-        if !v0.is_finite() || !v1.is_finite() || !v2.is_finite() {
-            return false;
-        }
-        if v0 == v1 || v1 == v2 || v2 == v0 {
-            return false;
-        }
-
-        // zero area check
-        // creates two edges making v0 the origin
-        let a = v1.substraction(v0);
-        let b = v2.substraction(v0);
-
-        // cross product of the two edges
-        // gives a vector orthogonal to the triangle
-        // whose length is proportional to the area of the triangle
-        let cross = a.cross(b);
-        // area squared is the dot product of the cross product with itself
-        let area_sq = cross.dot(cross);
-
-        area_sq > 1e-12
     }
 }
